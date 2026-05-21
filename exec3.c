64a1ae5b47eec31eb48ab96a96fec77759a4e466
Igor Ramon <digorramon06@gmail.com>
2026-05-21 14:06:29 -0300

    Add program to sum positive numbers from user input

---
+++ exec3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+int i, numero, soma = 0;
+
+for (i = 1; i <= 5; i++){
+    printf("Digite um numero: ");
+   scanf("%d", &numero);
+    if(numero > 0){
+        soma = soma + numero;
+    }   
+}
+printf("Soma: %d\n", soma);
+return 0;
+}
